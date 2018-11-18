d73d43327d016db9a7104689c270da2a24baf1ca
Phil Jones <interstar@gmail.com>
2018-11-18 14:31:58 -0200

    fixed broken unit tests

--- test/mtc_clj/core_test.clj
+++ test/mtc_clj/core_test.clj
@@ -16,22 +16,22 @@
       (is (= (count '()) 0))
       (is (= (count mtc) 3))
       (is (= (count mtc3) 5))
-      (is (= (next '()) "Mind Traffic Control is currently empty"))
-      (is (= (next mtc) "item 1"))
+      (is (= (next-item '()) "Mind Traffic Control is currently empty"))
+      (is (= (next-item mtc) "item 1"))
       (is (= (tail mtc) '("item 2" "item 3")))
 
       (is (= mtc2 '("item 1" "item 2" "item 3" "item 4")))
       (is (= mtc3 '("item 1" "item 2" "item 3" "item 4" "item 5")))
       (is (= (add-first mtc "item 0") '("item 0" "item 1" "item 2" "item 3")))
 
-      (is (= (delay mtc) '("item 2" "item 3" "item 1")))
+      (is (= (delay-item mtc) '("item 2" "item 3" "item 1")))
       (is (= (done mtc) '("item 2" "item 3")))
       ) ))
 
 (deftest defering
   (testing "delay"
     (let [mtc (range 10)]
-      (is (= (delay mtc 5) '(1 2 3 4 5 0 6 7 8 9) ))
+      (is (= (delay-item mtc 5) '(1 2 3 4 5 0 6 7 8 9) ))
       )))
 
 (deftest tags
